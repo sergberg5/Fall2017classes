c06d6becd3b433cc3c66ede522f1f5336288363f
sergberg5 <sergberg5@gmail.com>
2017-11-27 21:22:25 -0800

    finalized testing, ready to turn in

--- Linux_Class_Fall2017/blackJack/blackjack.sh
+++ Linux_Class_Fall2017/blackJack/blackjack.sh
@@ -31,27 +31,27 @@ player_hand(){
 		#echo $tempValue
 		cardValue=$(($cardValue + $tempPlayerValue))
 		echo ""
-		echo -n "Your current card value is " 
-		echo $cardValue;
+		echo -n " ["
+		echo -n $cardValue
+		echo "] "
 		count=$(($count + $counter))
 		echo ""
 		echo 'Do you want to hit or stay?'
 		echo "Enter h to hit and s to stay"
 		# echo $cardValue
 		# echo $count
+		accVal=print_account_value $1 2> /dev/null
 		if [ "$cardValue" -eq 21 ] && [ "$count" -eq 2 ]; then
 			echo "You got a Black Jack you Win!"
 			echo ""
+			add_bet
 			echo -n "You currently have "
 			print_account_value $1
 			blackJack=0
 			play_again
 		elif [ "$cardValue" -gt 21 ]; then
 			echo "You Lost :("
-
-			accVal=print_account_value $1 2> /dev/null
-			accVal=$(($accVal - $bet))
-			echo $accVal > ./Users/$1.txt
+			sub_bet
 			echo ""
 			echo -n "You currently have "
 			print_account_value $1
@@ -74,7 +74,7 @@ player_hand(){
 
 computer_hand(){
 	### BALLZY PLAY: Please input 15 18 or 20 into the variable $comp
-	comp=18
+	comp=17
 	echo -n "Calculating computer hand"
 	echo -n " ."
 	while true; do
@@ -82,35 +82,76 @@ computer_hand(){
 		echo -n " ."
 		#echo $tempValue
 		compCardValue=$(($compCardValue + $tempPlayerValue))
-		echo -n " ."
-
+		echo " ."
+		accVal=print_account_value $1 2> /dev/null
 		if [ "$compCardValue" -eq 21 ] && [ "$count" -eq 2 ]; then
 			echo "You lost :( Dealer got BlackJack"
 			echo ""
+			sub_bet
 			echo -n "You currently have "
 			print_account_value $1
 			blackJack=0
+			compCardValue=0
 			play_again
 		elif [ "$compCardValue" -gt 21 ]; then
 			echo "Dealer Bust: You Win"
-
-			accVal=print_account_value $1 2> /dev/null
-			accVal=$(($accVal + $bet * 2))
-			echo $accVal > ./Users/$1.txt
-			echo ""
+			add_bet
 			echo -n "You currently have "
 			print_account_value $1
 			blackJack=0
+			compCardValue=0
 			play_again
-		elif [ "$cardValue" -ge "$comp" ]; then
-			break
+
+		elif [ "$compCardValue" -ge "$comp" ] && [ "$compCardValue" -le 21 ]; then
+			if [ "$cardValue" -ge "$compCardValue" ]; then
+				echo -n " ["
+				echo -n $compCardValue
+				echo "] "
+				echo ""
+				echo "You Win"
+				add_bet
+				echo -n "You currently have "
+				print_account_value $1
+				blackJack=0
+				compCardValue=0
+				play_again
+			else
+				echo -n " ["
+				echo -n $compCardValue
+				echo "] "
+				echo "You Lose"
+				echo ""
+				sub_bet
+				echo -n "You currently have "
+				print_account_value $1
+				blackJack=0
+				compCardValue=0
+				play_again
+			fi
+				compCardValue=0
+		else
+			echo -n " ["
+			echo -n $compCardValue
+			echo "] "
 		fi
 	done
 
 }
 
+add_bet(){
+	bet=$(($bet * 2))
+	accVal=$(($accVal + $bet))
+	echo $accVal > ./Users/$username.txt
+}
+
+sub_bet(){
+	accVal=print_account_value $username 2> /dev/null
+	accVal=$(($accVal - $bet))
+	echo $accVal > ./Users/$username.txt
+}
+
 play_again(){
-	echo "Thanks for playing did you want to play again? (y/n)"
+	echo "Thanks for playing, did you want to play again? (y/n)"
 	read playAgain
 	case $playAgain in
 		"y")   
@@ -132,16 +173,15 @@ print_account_value() {				#account value import
 
 		search_user $username 
 		player_hand $username
-		
-		if [ "$blackJack" -ne 0 ]; then
-			computer_hand
-			if [ "$cardValue" -gt "$compCardValue"]
+		if [ "$blackJack" != 0 ]; then
+			computer_hand $username
 		else
 			echo "Thanks for playing did you want to play again? (y/n)"
 			read playAgain
 			case $playAgain in
 				"y")   
-	       			print_account_value $username;;
+					blackJack=1
+	       			player_hand $username;;
         		"n")
 	       			break;;
 			esac
